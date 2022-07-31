3e04aa8b40b284482f069fdefd360f8d99dea2a4
eladsadeh <elad.sadeh1@.gmail.com>
2022-07-31 16:58:56 -0400

    fix toggling of select list

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,7 +6,21 @@ import styles from '../components/Atoms/Select/Select.style'
 function Projects() {
   const [value, setValue] = useState<number>(0);
   const [showSelect, setShowSelect] = useState<boolean>(false)
-  const list = [{label: 'First option', value: 1}, {label: 'Second option', value: 2}, {label: 'Third option', value: 3}, {label: 'Fourth option', value: 4}];
+  const list: {label: string, value: number}[] = [
+    { label: "California", value: 1 },
+    { label: "New Hampshire", value: 2 },
+    { label: "Montana", value: 3 },
+    { label: "New York", value: 4 },
+    { label: "Colorado", value: 5 },
+    { label: "Arizona", value: 6 },
+    { label: "Washington", value: 7 },
+    { label: "Oregon", value: 8 },
+    { label: "Utah", value: 9 },
+    { label: "Alabama", value: 10 },
+    { label: "Hawaii", value: 11 },
+    { label: "Alaska", value: 12 },
+    { label: "Vermont", value: 13 }
+  ];
   const currentSelection = list.find((option) => 
     option.value === value);
 
@@ -18,7 +32,7 @@ function Projects() {
       <div css={styles.wrapper}>Select List Demo
         <label>Select list</label>
         <div className='input'
-          onClick={() => setShowSelect(true)}>
+          onClick={() => setShowSelect(!showSelect)}>
           {currentSelection?.label || 'Click to open list'}
           <Select 
             list={list} 
